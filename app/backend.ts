9b2b7428f641bc7349ccec303cb44451729b0d1d
vinassefranche <vincent.francois@inato.com>
2025-11-12 19:27:06 +0100

    Reduce fake time of server actions

--- app/backend.ts
+++ app/backend.ts
@@ -2,11 +2,11 @@
 
 let counter = 0
 export const getCounter = async () => {
-  await new Promise<void>(resolve => setTimeout(() => resolve(), 700))
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 10))
   return counter
 }
 
 export const increaseCounter = async () => {
-  await new Promise<void>(resolve => setTimeout(() => resolve(), 300))
+  await new Promise<void>(resolve => setTimeout(() => resolve(), 10))
   counter++
 }

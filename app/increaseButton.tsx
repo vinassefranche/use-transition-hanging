e6db821b416d047fb0c1914a1a3defe7df89659f
vinassefranche <vincent.francois@inato.com>
2025-11-12 20:49:57 +0100

    Make the button more visible

--- app/increaseButton.tsx
+++ app/increaseButton.tsx
@@ -7,7 +7,7 @@ export const IncreaseButton = () => {
   const router = useRouter()
   const [loading, startTransition] = useTransition();
   return (
-    <button className="bg-gray-200 p-1" onClick={() => {
+    <button className="bg-gray-200 px-4 py-1 rounded-xl hover:bg-gray-300 cursor-pointer" onClick={() => {
       startTransition(async () => {
         await increaseCounter();
         router.refresh()
